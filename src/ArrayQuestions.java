571c0df3a66e6f742780e0241679d597da6225e9
meldasav <gmeldag83@hotmail.com>
2022-06-04 21:32:35 -0500

    collections

---
+++ src/ArrayQuestions.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayQuestions {
+    public static void main(String[] args) {
+        HashMap<String,String> map=new HashMap<>();
+        map.put("USA","Washington DC");
+        map.put("Turkey","Ankara");
+        map.put("UK","London");
+        map.forEach((k,v)->System.out.println("key = " + k + " value= " + v ));
+        countCharacter("test");
+        countCharacter1("tester");
+        System.out.println(findMissingNumber(new int[]{1, 2, 3, 4, 6, 7, 8, 9, 10}, 10));
+        System.out.println(Arrays.toString(shiftZeroToRight(new int[]{1, 0, 2, 0, 3, 0, 0})));
+    }
+
+    public static void countCharacter(String sentence) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : sentence.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        System.out.println(map);
+    }
+
+    public static void countCharacter1(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> enter : map.entrySet()) {
+            System.out.println(enter);
+        }
+    }
+
+    //Find the missing number in array
+    public static int findMissingNumber(int[] num, int totalCount) {
+        /**
+         * 1+2+3+5=11
+         * 1+2+3+4+5=15
+         * formula=totalNumberOfElements(totalNumberOfElements+1)/2
+         * 5(5+1)/2=15;
+         * 15-11=4;
+         */
+        int expSum = totalCount * (totalCount + 1) / 2;//formula
+        int actualSum = 0;
+        for (int i : num) {
+            actualSum += i;
+        }
+        return expSum - actualSum;
+    }
+
+    public static int[] shiftZeroToRight(int[] a) {
+        if (a.length == 1) return a;
+        else {
+            int[] array = new int[a.length];
+            int count = 0;
+            for (int number : a) {
+                if (number != 0) array[count++] = number;
+            }
+            return array;
+        }
+    }
+
+
+
+}
---
+++ src/_05HashMap.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class _05HashMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> compMap = new HashMap<>();
+        compMap.put("Google", 10000);
+        compMap.put("Walmart", 20000);
+        compMap.put("Amazon", 30000);
+        compMap.put("Facebook", 5000);
+        compMap.put("Sysco", 15000);
+        compMap.forEach((k, v) -> System.out.println("key value = " + k + " value = " + v));
+        System.out.println(compMap.keySet());
+        System.out.println(compMap.entrySet());
+        System.out.println(compMap);
+        System.out.println(compMap(compMap));
+        System.out.println(compMap1(compMap));
+
+    }
+
+    public static ArrayList<Integer> compMap(HashMap<String, Integer> map) {
+        ArrayList<Integer> newList = new ArrayList<>((map.values()));
+        for (Integer list : newList) {
+            if (!newList.contains(list)) newList.add(list);
+        }
+        return newList;
+    }
+
+    public static ArrayList<String> compMap1(HashMap<String, Integer> map) {
+        ArrayList<String> newList = new ArrayList<>((map.keySet()));
+        for (String list : newList) {
+            if (!newList.contains(list)) newList.add(list);
+        }
+        return newList;
+
+    }
+}
