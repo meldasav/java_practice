715e632921ad5ebd4ea8c43b8c6b38e0b96fa2c7
meldasav <gmeldag83@hotmail.com>
2022-06-13 07:16:42 -0500

    hashMap Practice

---
+++ src/CountDuplicatesInString.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class CountDuplicatesInString {
+    public static void main(String[] args) {
+        countDuplicates("i am am learning java java");
+        countDuplicateCharacter(("jajaja"));
+    }
+
+    //TODO (the word java appeared 2 times)
+    //TODO (the word am appeared 2 times)
+
+    public static void countDuplicates(String str) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String s : str.split(" ")) {
+            if (map.containsKey(s)) map.put(s, map.get(s) + 1);
+            else map.put(s, 1);
+        }
+        for (String temp : map.keySet()) {
+            if (map.get(temp) > 1) System.out.println("the word " + temp + " appeared " + map.get(temp) + " times");
+        }
+    }
+
+    //TODO The character a appeared 3 times
+    //TODO The character j appeared 3 times
+
+    public static void countDuplicateCharacter(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (Character c : str.toCharArray()) {
+            if (map.containsKey(c)) map.put(c, map.get(c) + 1);
+            else map.put(c, 1);
+        }
+        for (Character c : map.keySet()) {
+            if (map.get(c) > 1) System.out.println("The character " + c + " appeared " + map.get(c) + " times");
+        }
+    }
+
+
+}
