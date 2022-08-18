60e15186e639693d8e8f16b92080411e9bfd41e3
Melda Sav <gmeldag83@hotmail.com>
2022-08-18 12:46:15 -0500

    practice

---
+++ src/interview/Max_Sum_Of_Array.java
@@ -0,0 +1,25 @@
+package interview;
+
+public class Max_Sum_Of_Array {
+
+    public static void main(String[] args) {
+        System.out.println(maxSum(new int[]{1,2,3,4,5},2));
+    }
+
+
+    public static int maxSum(int[] array ,int k){
+        int sum=0;
+        for (int i = 0; i < k; i++) {
+            sum+=array[i];
+        }
+        int maxSum=0;
+        int start=0;
+        for (int i = k; i <array.length ; i++) {
+            sum+=array[i]-array[start++];
+            if(sum>maxSum)maxSum=sum;
+        }
+        return maxSum;
+    }
+}
+
+
