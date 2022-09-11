041bad1483c76ce56a77c7f5c42136e7999f255f
Melda Sav <gmeldag83@hotmail.com>
2022-09-11 08:51:40 -0500

    Arrays.copyOfRange method

--- src/interview/array_practice/Copy_Array.java
+++ src/interview/array_practice/Copy_Array.java
@@ -9,5 +9,13 @@ public class Copy_Array {
 
         System.out.println("Array1 = " + Arrays.toString(array1));
         System.out.println("Array2 = " + Arrays.toString(array2));
+
+
+        int[] numbers1 = {1, 2, 3, 4};
+        int[] numbers2 = Arrays.copyOfRange(numbers1, 0, numbers1.length);
+
+        System.out.println("numbers1 = " + Arrays.toString(numbers1));
+        System.out.println("numbers2 = " + Arrays.toString(numbers2));
+
     }
 }
