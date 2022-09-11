776d620f79a83b7550b13ac98e0585096483d364
Melda Sav <gmeldag83@hotmail.com>
2022-09-11 08:59:46 -0500

    Arrays.copyOfRange method

--- src/interview/array_practice/Copy_Array.java
+++ src/interview/array_practice/Copy_Array.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Copy_Array {
     public static void main(String[] args) {
         String[] array1 = {"Monday", "Tuesday", "Wednesday"};
+
+        //Arrays.copyOfRange give a range start from index of 0 -> array size
         String[] array2 = Arrays.copyOfRange(array1, 0, array1.length);
 
         System.out.println("Array1 = " + Arrays.toString(array1));
