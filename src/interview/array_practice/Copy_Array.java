b025d7ba590e9f2cb70a31a3af09ceed5dddf360
Melda Sav <gmeldag83@hotmail.com>
2022-09-11 22:45:46 -0500

    scanner practice

--- src/interview/array_practice/Copy_Array.java
+++ src/interview/array_practice/Copy_Array.java
@@ -1,6 +1,7 @@
 package interview.array_practice;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Copy_Array {
     public static void main(String[] args) {
@@ -18,6 +19,19 @@ public class Copy_Array {
 
         System.out.println("numbers1 = " + Arrays.toString(numbers1));
         System.out.println("numbers2 = " + Arrays.toString(numbers2));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter 2 numbers");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        System.out.println("The number 1 entered by user is = " + number1);
+        System.out.println("The number 2 entered by user is = " + number2);
+        System.out.println("The sum of number 1 and number 2 entered by user is = " + (double) (number1 + number2));
+        System.out.println("The product of the given 2 number is : " + (double) (number1 * number2));
+        System.out.println("The subtraction of the given number is = " + (double) (number1 - number2));
+        System.out.println("The division of the given 2 numbers is = " + (float) (number1 / number2));
+        System.out.println("The remainder of the given number is = " + (double) (number1 % number2));
+        System.out.println("The average of the given 2 number is = " + (number1 + number2) / 2);
 
     }
 }
