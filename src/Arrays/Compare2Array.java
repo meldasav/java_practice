89d081540b1f55a9a80af92776003fe831089cbc
meldasav <gmeldag83@hotmail.com>
2022-10-31 11:17:53 -0500

    practice

--- src/Arrays/Compare2Array.java
+++ src/Arrays/Compare2Array.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Compare2Array {
     public static void main(String[] args) {
+
         int[] array1 = {5, 8, 13, 1, 2};
         int[] array2 = {9, 3, 68, 1, 5};
         int[] array3 = new int[5];
