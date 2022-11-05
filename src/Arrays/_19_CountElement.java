4f061a8f23735fa538c33d8e414b7fc40394bfef
meldasav <gmeldag83@hotmail.com>
2022-11-05 13:36:13 -0500

    practice

--- src/Arrays/_19_CountElement.java
+++ src/Arrays/_19_CountElement.java
@@ -5,7 +5,9 @@ import java.util.Vector;
 
 public class _19_CountElement {
     public static void main(String[] args) {
-        Vector<Integer> vector=new Vector<>(Arrays.asList(10,15,20,25,30));
-        System.out.println(vector.stream().filter(x->x%2==0).count());
+        Vector<Integer> vector = new Vector<>(Arrays.asList(10, 15, 20, 25, 30));
+        System.out.println(vector.stream().filter(x -> x % 2 == 0).count());
+        System.out.println(vector.stream().filter(x -> x % 2 == 1).count());
+        System.out.println(vector.stream().filter(x -> x > 20).count());
     }
 }
