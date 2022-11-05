0e1994f14c153fa932bef5a8f1c567417aa83c3b
meldasav <gmeldag83@hotmail.com>
2022-11-05 09:57:51 -0500

    practice

---
+++ src/Arrays/_18_RemoveElementFromLinkList.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class _18_RemoveElementFromLinkList {
+    public static void main(String[] args) {
+        LinkedList<String> languages = new LinkedList<>(Arrays.asList("Java", "#C", "Java Script", "Python"));
+        languages.removeIf(x -> x.toLowerCase().startsWith("j"));
+        System.out.println(languages);
+        LinkedList<String> list1 = new LinkedList<>();
+        for (String language : languages) {
+            if (language.toLowerCase().startsWith("j")) {
+                continue;
+            }
+            list1.add(language);
+        }
+        System.out.println(list1);
+    }
+}
