d326ec40cfea38fdee24a86fcb8b8e6c3ddb38c6
meldasav <gmeldag83@hotmail.com>
2022-11-04 22:32:47 -0500

    practice

--- src/Arrays/_12_FindSecondSmallestAndLargest.java
+++ src/Arrays/_12_FindSecondSmallestAndLargest.java
@@ -14,7 +14,24 @@ public class _12_FindSecondSmallestAndLargest {
             System.out.println("Second largest : " + list.get(list.size() - 2));
         }
     }
+
+    public static void findSecondLargestAndSmallest1(int[] numbers) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, secondMin = Integer.MAX_VALUE, secondMax = Integer.MIN_VALUE;
+        for (int number : numbers) {
+            if (number < min) min = number;
+            if (number > max) max = number;
+        }
+        for (int number : numbers) {
+            if (number != min && number < secondMin) secondMin = number;
+            if (number != max && number > secondMax) secondMax = number;
+        }
+        System.out.println("Second smallest : " + secondMin);
+        System.out.println("Second largest : " + secondMax);
+    }
+
     public static void main(String[] args) {
         findSecondLargestAndSmallest(new int[]{1, 1, 2, 3, 4, 5});
+        findSecondLargestAndSmallest1(new int[]{1, 1, 2, 3, 4, 5});
+
     }
 }
