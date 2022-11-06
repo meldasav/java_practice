2442824aa0c19115fd62c5cf96db0b5d19ae3279
meldasav <gmeldag83@hotmail.com>
2022-11-06 11:23:58 -0600

    practice

--- src/Arrays/_02_intArray.java
+++ src/Arrays/_02_intArray.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class _02_intArray {
     public static void main(String[] args) {
-        System.out.println(sumOf3(new int[]{1,2,3,4,5}));
+        System.out.println(sumOfLast5(new int[]{1, 2, 3, 4, 5, 1, 2}));
+        shortestLongest(new String[]{"RED", "BLUE", "YELLOW"});
 
 
         int[] array = {0, -5, 2, 10, 5, 2, -1, 0};
@@ -25,5 +26,29 @@ public class _02_intArray {
             sum += array[i];
         }
         return sum;
+    }
+
+    public static int sumOfLast5(int[] array) {
+        int sum = 0;
+        for (int i = array.length - 1; i > array.length - 6; i--) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static void shortestLongest(String[] array) {
+        String shortest = array[0];
+        String longest = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (longest.length() < array[i].length()) {
+                longest = array[i];
+            }
+            if (shortest.length() > array[i].length()) {
+                shortest = array[i];
+            }
+        }
+        System.out.println("Longest : " + longest);
+        System.out.println("Shortest : " + shortest);
     }
 }
