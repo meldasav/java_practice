45f1b28a86b490cf3296ff74cbb5a80318fd7fd0
meldasav <gmeldag83@hotmail.com>
2022-12-05 07:19:45 -0600

    practice

--- src/Arrays/project/Project08.java
+++ src/Arrays/project/Project08.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 
 public class Project08 {
     public static void main(String[] args) {
-        
+
         System.out.println(findClosestDistance(new int[]{4}));
         System.out.println(findClosestDistance(new int[]{4, 4}));
         System.out.println(findClosestDistance(new int[]{4, 8, 10, 15}));
         System.out.println(findClosestDistance(new int[]{-5, 10, 20, 50, 100}));
+        System.out.println(noSpace1("Melda Sav"));
+        System.out.println(replaceFirstLast("SavMelda"));
+        System.out.println(vowel("SavMelda"));
+        System.out.println(vowel("SvMld"));
+
     }
 
     public static int findClosestDistance(int[] array) {
@@ -22,5 +27,37 @@ public class Project08 {
             }
             return closestDistance;
         }
+    }
+
+    public static String noSpace(String str) {
+        return str.replace(" ", "");
+    }
+
+    public static String noSpace1(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isSpaceChar(str.charAt(i))) {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public static String replaceFirstLast(String str) {
+        if (str.length() < 2) return "";
+        else str = str.substring(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
+        return str;
+    }
+
+    public static boolean vowel(String str) {
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i'
+                || c == 'u' || c == 'o') {
+                return true;
+            }
+        }
+        return false;
     }
 }
