63377b6d93bb18efdd45f1ceb760bc0749961f5c
meldasav <gmeldag83@hotmail.com>
2022-12-26 18:23:54 -0600

    Exceptions

---
+++ src/exceptions/try_catch_01.java
@@ -0,0 +1,29 @@
+package exceptions;
+
+import java.util.Scanner;
+
+public class try_catch_01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Please enter 2 numbers");
+            int number1 = scanner.nextInt();
+            int number2 = scanner.nextInt();
+
+            while (number1 == 0) {
+                System.out.println("Zero is not allowed.Enter another number");
+                number1 = scanner.nextInt();
+            }
+            System.out.println(number1 / number2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            System.out.println("End of program");
+        }
+    }
+}
